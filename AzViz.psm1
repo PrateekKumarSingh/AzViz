9183b5566f89cf0b449d1149614c0344e20fbdb5
prateekkumarsingh <prateeksingh1590@gmail.com>
2019-10-15 09:43:04 -0700

    add PSGraph Dependency

--- AzViz.psm1
+++ AzViz.psm1
@@ -1,4 +1,19 @@
-$Path = [System.IO.Path]::Combine($PSScriptRoot,'src')
+$Path = [System.IO.Path]::Combine($PSScriptRoot, 'src')
 Get-Childitem $Path -Filter *.ps1 -Recurse | Foreach-Object {
     . $_.Fullname
 }
+
+
+# verify dependent modules are loaded
+$DependentModules = 'PSGraph', 'az'
+$Installed = Import-Module $DependentModules -PassThru -ErrorAction SilentlyContinue | Where-Object { $_.name -In $DependentModules }
+$missing = $DependentModules | Where-Object { $_ -notin $Installed.name }
+if ($missing) {
+    Write-host "    [+] Module dependencies not found [$missing]. Attempting to install." -ForegroundColor Green
+    Install-Module $missing -Force -AllowClobber -Confirm:$false -Scope CurrentUser
+    Import-Module $missing
+}
+
+# Install GraphViz from the Chocolatey repo
+Register-PackageSource -Name Chocolatey -ProviderName Chocolatey -Location http://chocolatey.org/api/v2/ -ErrorAction SilentlyContinue -Verbose
+Find-Package graphviz | Install-Package -ForceBootstrap -Verbose
